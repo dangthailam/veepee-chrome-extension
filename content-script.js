de4bd86b77d03802e50aa3bdc2a4951f19fce621
dangthailam <lamtp1989@gmail.com>
2021-10-31 08:58:01 +0100

    get product detail

--- content-script.js
+++ content-script.js
@@ -17,7 +17,23 @@ expressBuyButtons.forEach(button => {
 });
 
 function exportArticle() {
-    console.log(this);
+    const article = this.closest('article.sales__sc-156l7nn-0');
+    const product = article.querySelector('header .sales__sc-156l7nn-5 a');
+    const productDetailUrl = product.href;
+    const productName = product.textContent;
+    let productOriginalPrice = article.querySelector('header .sc-ejmJqG').textContent;
+    let productFinalPrice = article.querySelector('header .sc-ibKkBv').textContent;
+    const productSalesPercent = article.querySelector('header .sc-hYQoXb').textContent;
+    const currency = productFinalPrice.split(' ')[1];
+    productOriginalPrice = Number(productOriginalPrice.split(' ')[0].replace(',', '.'));
+    productFinalPrice = Number(productFinalPrice.split(' ')[0].replace(',', '.'));
+    console.log(productDetailUrl);
+    console.log(productName);
+    console.log(productOriginalPrice);
+    console.log(productFinalPrice);
+    console.log(productSalesPercent);
+    console.log(currency);
+
 }
 
 
